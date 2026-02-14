5aef65c6a7cceccd99c7140ab2b6b33978d7bb15
Frook <sixingt123@gmail.com>
2026-02-14 13:45:50 -0800

    Add test item Add Item class Add assets folder

--- src/client/java/com/example/hackcraft_jan_2026/client/Hackcraft_jan_2026Client.java
+++ src/client/java/com/example/hackcraft_jan_2026/client/Hackcraft_jan_2026Client.java
@@ -4,6 +4,9 @@ import net.fabricmc.api.ClientModInitializer;
 
 public class Hackcraft_jan_2026Client implements ClientModInitializer {
 
+
+
+
     @Override
     public void onInitializeClient() {
     }
--- src/main/java/com/example/hackcraft_jan_2026/Hackcraft_jan_2026.java
+++ src/main/java/com/example/hackcraft_jan_2026/Hackcraft_jan_2026.java
@@ -1,10 +1,16 @@
 package com.example.hackcraft_jan_2026;
 
+import com.example.hackcraft_jan_2026.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Hackcraft_jan_2026 implements ModInitializer {
+    public static final String MOD_ID = "endupdatereal";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
+        ModItems.initializeModItems();
     }
 }
