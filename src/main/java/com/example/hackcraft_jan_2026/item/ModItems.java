fbf683e7f969ddcc225b000fe521b4be8c10cebd
Frook <sixingt123@gmail.com>
2026-02-14 17:49:08 -0800

    Change item registering function

--- src/main/java/com/example/hackcraft_jan_2026/item/ModItems.java
+++ src/main/java/com/example/hackcraft_jan_2026/item/ModItems.java
@@ -10,19 +10,29 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
 
 
 public class ModItems {
 
-    public static final Item TEST_ITEM = registerItem("test_item", new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Hackcraft_jan_2026.MOD_ID,"test_item")))));
 
+//    private static Item registerItem(String name, Item item) {
+//        return Registry.register(Registries.ITEM, Identifier.of(Hackcraft_jan_2026.MOD_ID), item);
+//
+//    }
 
+    public static <GenericItem extends Item> GenericItem registerItem(String name, Function<Item.Settings, GenericItem> itemFactory, Item.Settings settings) {
 
+        RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Hackcraft_jan_2026.MOD_ID, name));
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(Hackcraft_jan_2026.MOD_ID), item);
+        GenericItem item = itemFactory.apply(settings.registryKey(itemKey));
 
+        Registry.register(Registries.ITEM, itemKey, item);
+
+        return item;
     }
+
+    public static final Item TEST_ITEM = registerItem("test_item", Item::new, new Item.Settings());
 
 
 
